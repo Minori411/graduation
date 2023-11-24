593db602433844796cf7b1461b5a33f39d7db114
MINORI HAYASHI <hayashi.minori@jgc.com>
2023-11-24 17:00:26 +0900

    backend2

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -1,30 +1,21 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const { env } = require('process');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { env } = require("process");
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:13765';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+  ? env.ASPNETCORE_URLS.split(";")[0]
+  : "http://localhost:<ポート番号>";
 
-const context = [
-  "/weatherforecast",
-];
-
-const onError = (err, req, resp, target) => {
-    console.error(`${err.message}`);
-}
+const context = ["/api/"];
 
 module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
-    proxyTimeout: 10000,
     target: target,
-    // Handle errors to prevent the proxy middleware from crashing when
-    // the ASP NET Core webserver is unavailable
-    onError: onError,
     secure: false,
-    // Uncomment this line to add support for proxying websockets
-    //ws: true, 
     headers: {
-      Connection: 'Keep-Alive'
-    }
+      Connection: "Keep-Alive",
+    },
   });
 
   app.use(appProxy);
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using todo_react_app.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("TodoContext")));
 
 var app = builder.Build();
 
