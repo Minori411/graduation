e5ff39aa08b75c8fcaa504ef1e85f9e5178d2477
MINORI HAYASHI <hayashi.minori@jgc.com>
2023-11-28 15:32:13 +0900

    signup login posswordreset add

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -1,21 +1,27 @@
-// import { Todo } from "./components/Todo";
-import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/Login';
+import SignUppage from './components/SignUp'
+import { Suspense } from 'react';
 import TodoList from './components/TodoList';
+import PasswordReset from './components/PasswordReset'
 import { RecoilRoot } from 'recoil';
 import TodoAppBar from './components/TodoAppBar';
+import ReactDOM from "react-dom";
 
 export default function App() {
   return (
-    <RecoilRoot>
-      <div className="App">
-        <TodoAppBar />
-        <TodoList/>
-      </div>
-    </RecoilRoot>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/passwordreset" element={<PasswordReset />} />
+          <Route path="/signup" element={<SignUppage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<RecoilRoot><div className="App"><TodoAppBar /><TodoList /></div></RecoilRoot>} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
 const rootElement = document.getElementById("root")!;
-const root = createRoot(rootElement);
-
-root.render(<App />);
+ReactDOM.render(<App />, rootElement);
