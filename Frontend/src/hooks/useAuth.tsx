15a4d9c5441e3aa78e4df60ecb49a6c9e59a8cdd
MINORI HAYASHI <hayashi.minori@jgc.com>
2023-12-04 10:07:26 +0900

    accesstoken add

---
+++ Frontend/src/hooks/useAuth.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { useMsal } from "@azure/msal-react";
+
+function MyComponent() {
+  const { instance, accounts } = useMsal();
+
+  useEffect(() => {
+    if (accounts.length > 0) {
+      instance.acquireTokenSilent({
+        scopes: ["user.read"],
+        account: accounts[0]
+      }).then(response => {
+        // トークンを使用してAPIリクエストを行う
+        fetch("https://localhost:7256", {
+          headers: {
+            Authorization: `Bearer ${response.accessToken}`
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      }).catch(error => {
+        // トークン取得エラーのハンドリング
+        console.error(error);
+      });
+    }
+  }, [instance, accounts]); // 依存配列にinstanceとaccountsを追加
+
+  return <div>コンテンツ</div>;
+}
+
+export default MyComponent;
